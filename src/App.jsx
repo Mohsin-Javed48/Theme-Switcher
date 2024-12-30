37b7ca66ee185792d4d195b71640b46705020afc
Mohsin-Javed <mohsinjavedpc@gmail.com>
2024-12-30 19:40:03 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+/** @format */
+
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import { ThemeProvider } from "./contexts/Theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("");
+  console.log(themeMode);
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  useEffect(() => {
+    const windowTheme = document.querySelector("html");
+    windowTheme.classList.remove("dark", "light");
+    if (themeMode) windowTheme.classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
